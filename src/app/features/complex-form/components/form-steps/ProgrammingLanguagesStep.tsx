3a4436f8f44d5b1b7f0297402a3ebea5e2051df8
Clément Touzet <clement.touzet40@gmail.com>
2025-08-17 21:54:46 +0200

    added libraries but they are still not included in the form data

--- src/app/features/complex-form/components/form-steps/ProgrammingLanguagesStep.tsx
+++ src/app/features/complex-form/components/form-steps/ProgrammingLanguagesStep.tsx
@@ -1,14 +1,17 @@
 import {
   Button,
+  ButtonGroup,
   Card,
   CardBody,
   CardHeader,
   Chip,
+  Input,
   Select,
   SelectItem,
 } from "@heroui/react";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import {
+  HiCheck,
   HiOutlineTrash,
   HiOutlineXCircle,
   HiPlus,
@@ -21,7 +24,7 @@ const ProgrammingLanguagesStep = () => {
   const [selectedLanguages, setSelectedLanguages] = useState(["JavaScript"]);
   const [isLanguagesSelectOpen, setIsLanguagesSelectOpen] = useState(false);
 
-  const notSelectedLanguages = ["C++", "Java"].filter(
+  const notSelectedLanguages = ["C++", "Java", "JavaScript"].filter(
     (language) => !selectedLanguages.includes(language)
   );
 
@@ -33,6 +36,12 @@ const ProgrammingLanguagesStep = () => {
 
   const addToSelectedLanguage = (value: string) => {
     setSelectedLanguages((prev) => [...prev, value]);
+  };
+
+  const deleteLanguage = (languageToDelete: string) => {
+    setSelectedLanguages((prev) =>
+      prev.filter((language) => languageToDelete !== language)
+    );
   };
 
   const handleClickAddLanguage = () => {
@@ -51,45 +60,48 @@ const ProgrammingLanguagesStep = () => {
 
   return (
     <>
-      <div className="  ">
-        <h2 className="font-bold pb-2">Compétences en programmation</h2>
+      <div className="flex flex-col gap-4">
+        <div>
+          <h2 className="font-bold">Compétences en programmation</h2>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
           {selectedLanguages.map((language) => (
-            <ProgrammingSkillCard language={language} key={language} />
+            <ProgrammingSkillCard
+              language={language}
+              key={language}
+              deleteLanguage={deleteLanguage}
+            />
           ))}
-        </div>
-
-        {showAddProgrammingLanguageButton ? (
-          <Button
-            color="primary"
-            startContent={<HiPlus />}
-            onPress={handleClickAddLanguage}
-          >
-            Ajouter une compétence
-          </Button>
-        ) : null}
-
-        {showLanguagesSelect ? (
-          <>
-            <Select
-              label="Langage"
-              onChange={handleSelectProgrammingLanguage}
-              isOpen={isLanguagesSelectOpen}
-              onOpenChange={(open) => {
-                console.log("open", open, isLanguagesSelectOpen);
-                // default select open/close behavior.
-                return (
-                  open !== isLanguagesSelectOpen &&
-                  setIsLanguagesSelectOpen(open)
-                );
-              }}
+          {showAddProgrammingLanguageButton ? (
+            <Button
+              color="primary"
+              startContent={<HiPlus />}
+              onPress={handleClickAddLanguage}
             >
-              {notSelectedLanguages.map((language) => (
-                <SelectItem key={language}>{language}</SelectItem>
-              ))}
-            </Select>
-          </>
-        ) : null}
+              Ajouter une compétence
+            </Button>
+          ) : null}
+          {showLanguagesSelect ? (
+            <>
+              <Select
+                label="Langage"
+                onChange={handleSelectProgrammingLanguage}
+                isOpen={isLanguagesSelectOpen}
+                onOpenChange={(open) => {
+                  // default select open/close behavior.
+                  return (
+                    open !== isLanguagesSelectOpen &&
+                    setIsLanguagesSelectOpen(open)
+                  );
+                }}
+              >
+                {notSelectedLanguages.map((language) => (
+                  <SelectItem key={language}>{language}</SelectItem>
+                ))}
+              </Select>
+            </>
+          ) : null}
+        </div>
       </div>
     </>
   );
@@ -97,36 +109,146 @@ const ProgrammingLanguagesStep = () => {
 
 export default ProgrammingLanguagesStep;
 
-const ProgrammingSkillCard = ({ language }: { language: string }) => {
+type ProgrammingSkillCardProps = {
+  language: string;
+  deleteLanguage: (language: string) => void;
+};
+
+const ProgrammingSkillCard = ({
+  language,
+  deleteLanguage,
+}: ProgrammingSkillCardProps) => {
+  const onDeleteLanguageClick = () => {
+    deleteLanguage(language);
+  };
+
+  const [showAddLibraryInput, setShowAddLibraryInput] = useState(false);
+  const [libraries, setLibraries] = useState(["Tailwind", "MUI", "Next.js"]);
+  const [newLibrary, setNewLibrary] = useState("");
+
+  const refInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (showAddLibraryInput) refInput.current?.focus();
+  }, [showAddLibraryInput]);
+
+  const showAddLibraryButton = !showAddLibraryInput;
+
+  const addLibrary = () => {
+    setLibraries((prev) => [...prev, newLibrary]);
+    resetNewLibrary();
+  };
+
+  const resetNewLibrary = () => {
+    setNewLibrary("");
+  };
+
+  const hideAddLibraryInput = () => {
+    setShowAddLibraryInput(false);
+  };
+
+  const onClickAddLibrary = () => {
+    setShowAddLibraryInput(true);
+  };
+
+  const handleCancelAddLibrary = () => {
+    hideAddLibraryInput();
+  };
+
+  const handleAddNewLibrary = () => {
+    addLibrary();
+    hideAddLibraryInput();
+  };
+
+  const handleDeleteLibrary = (libraryToDelete: string) => {
+    setLibraries((prev) =>
+      prev.filter((library) => libraryToDelete !== library)
+    );
+  };
+
+  const handleChangeNewLibrary = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewLibrary(event.target.value);
+  };
+
   return (
     <Card>
-      <CardHeader className="flex justify-between">
+      <CardHeader className="flex justify-between pb-0">
         <h3 className="font-semibold pb-0.5">{language}</h3>
         <Button
           isIconOnly
           startContent={<HiXMark />}
           size="sm"
           variant="light"
+          onPress={onDeleteLanguageClick}
         />
       </CardHeader>
-      <CardBody>
+      <CardBody className="gap-1">
         <div className="flex flex-wrap gap-1">
-          <Chip
-            variant="flat"
-            endContent={
-              <HiOutlineXCircle className="hover:text-red-500 cursor-pointer" />
-            }
-          >
-            Tailwind
-          </Chip>
-          <Chip
-            className="pl-2 cursor-pointer"
-            color="primary"
-            startContent={<HiPlus />}
-          >
-            Bilibothèque
-          </Chip>
+          {libraries.map((library) => {
+            return (
+              <Chip
+                variant="flat"
+                key={library}
+                onClose={() => handleDeleteLibrary(library)}
+              >
+                {library}
+              </Chip>
+            );
+          })}
+          {showAddLibraryButton ? (
+            <Chip
+              className="pl-2 cursor-pointer"
+              color="primary"
+              startContent={<HiPlus />}
+              onClick={onClickAddLibrary}
+              variant="light"
+            >
+              Bibliothèque
+            </Chip>
+          ) : null}
         </div>
+        {showAddLibraryInput ? (
+          <div className="flex items-center gap-0.5">
+            <Button
+              size="sm"
+              variant="light"
+              radius="full"
+              isIconOnly
+              onPress={handleCancelAddLibrary}
+            >
+              <HiXMark />
+            </Button>
+            <ButtonGroup className="justify-start">
+              <Input
+                // size="sm"
+                classNames={{
+                  inputWrapper: "rounded-r-none",
+                }}
+                className="max-w-30"
+                placeholder="Biliothèque"
+                minLength={1}
+                onChange={handleChangeNewLibrary}
+                value={newLibrary}
+                ref={refInput}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    handleAddNewLibrary();
+                  }
+                }}
+              ></Input>
+              <Button
+                // size="sm"
+                color="primary"
+                onPress={handleAddNewLibrary}
+              >
+                Ajouter
+              </Button>
+            </ButtonGroup>
+          </div>
+        ) : null}
       </CardBody>
     </Card>
   );
